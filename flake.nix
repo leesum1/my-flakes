3d45f794dbcd57775e5c611912c3795af77b0af5
leesum <1255273338@qq.com>
2024-09-17 17:07:53 +0800

    update

--- flake.nix
+++ flake.nix
@@ -1,36 +1,24 @@
 {
-  description = "Flake for combining riscv-toolchain-flakes and sbt-overlay-flakes";
+  description = "Aggregated flake with sbt-overlay-flakes and riscv-toolchain-flakes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # 引入 riscv-toolchain-flakes
-    # 用 follows 引用相对路径的 flake
-    riscv-toolchain-flakes.follows = "self";
-    sbt-overlay-flakes.follows = "self";
-
     flake-utils.url = "github:numtide/flake-utils";
+
+    # 引用子 Flakes
+    sbt-overlay-flakes.url = "path:./sbt-overlay-flakes";
+    riscv-toolchain-flakes.url = "path:./riscv-toolchain-flakes";
   };
 
-  outputs = { self, nixpkgs, riscv-toolchain-flakes, sbt-overlay-flakes, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system: 
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        riscv-toolchain = riscv-toolchain-flakes.packages.${system};
+  outputs = { self, nixpkgs, flake-utils, sbt-overlay-flakes, riscv-toolchain-flakes }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = {
+        # 导出 sbt-overlay-flakes 的包
         sbt = sbt-overlay-flakes.packages.${system}.sbt;
-      in
-      {
-        packages = {
-          # 将 riscv-toolchain 和 sbt 组合在一起
-          riscv-toolchain = riscv-toolchain;
-          sbt = sbt;
-        };
 
-        # # 如果需要默认导出某个包
-        # defaultPackage = pkgs.buildEnv {
-        #   name = "combined-env";
-        #   paths = [ riscv-toolchain sbt ];
-        # };
-      }
-    );
+        # 导出 riscv-toolchain-flakes 的包
+        riscv-gcc = riscv-toolchain-flakes.packages.${system}.riscv-gcc;
+        riscv-gdb = riscv-toolchain-flakes.packages.${system}.riscv-gdb;
+      };
+    });
 }
